e547e2be4f3467228ec4909a9fe0eabc1f8cf588
Sebastian Bensusan <sbensu@gmail.com>
2017-02-19 14:41:05 -0800

    parse defns args

--- src/spec_sugar/core.clj
+++ src/spec_sugar/core.clj
@@ -2,30 +2,9 @@
   (:require [clojure.spec :as s]
             [clojure.core.specs :as cs]))
 
-(defn is-spec?
-  "returns true if spec-or-k is a spec, predicate, regex or resolvable kw/sym"
-  [spec-or-k]
-  (boolean (or (and (ident? spec-or-k)
-                    (and (keyword? spec-or-k) (some? (namespace spec-or-k))))
-               (s/spec? spec-or-k)
-               (s/regex? spec-or-k)
-               (symbol? spec-or-k)
-               (try
-                 (s/spec spec-or-k)
-                 (catch Exception _ false)))))
-
-(s/def ::spec is-spec?)
-
-(s/def ::typed-binding-form
-  (s/cat :form ::cs/binding-form
-         :separator #{:-}
-         :type ::spec))
-
-(s/def ::maybe-typed-args
-  (s/* (s/alt :untyped ::cs/binding-form :typed ::typed-binding-form)))
 
 (comment
-
+  ;; Reference
 
   (s/def ::local-name (s/and simple-symbol? #(not= '& %)))
 
@@ -34,26 +13,54 @@
           :seq ::seq-binding-form
           :map ::map-binding-form))
 
-
-
   (s/def ::arg-list
     (s/and
      vector?
      (s/cat :args (s/* ::binding-form)
             :varargs (s/? (s/cat :amp #{'&} :form ::binding-form)))))
+)
 
-  (s/def ::args+body
-    (s/cat :args ::arg-list
-           :prepost (s/? map?)
-           :body (s/* any?))))
+(defn is-spec?
+  "returns true if spec-or-k is a spec, predicate, regex or resolvable kw/sym"
+  [spec-or-k]
+  (boolean (or (and (ident? spec-or-k)
+                    (and (keyword? spec-or-k) (some? (namespace spec-or-k))))
+               (s/spec? spec-or-k)
+               (s/regex? spec-or-k)
 
-(s/def ::cs/defn-args
+               (symbol? spec-or-k)
+               (try
+                 (s/spec spec-or-k)
+                 (catch Exception _ false)))))
+
+(s/def ::spec is-spec?)
+
+(s/def ::separator #{:-})
+
+(s/def ::typed-binding-form
+  (s/cat :form ::cs/binding-form
+         :separator ::separator
+         :type ::spec))
+
+(s/def ::maybe-typed-args
+  (s/* (s/alt :untyped ::cs/binding-form :typed ::typed-binding-form)))
+
+(s/def ::args+body
+  (s/cat :args (s/and vector? (s/spec ::maybe-typed-args))
+         :prepost (s/? map?)
+         :body (s/* any?)))
+
+(s/def ::bodies
+  (s/alt :arity-1 ::args+body
+         :arity-n (s/cat :bodies (s/+ (s/spec ::args+body))
+                         :attr (s/? map?))))
+
+(s/def ::defn-args
   (s/cat :name simple-symbol?
+         :ret (s/? (s/cat :sep ::separator :ret-spec ::spec))
          :docstring (s/? string?)
          :meta (s/? map?)
-         :bs (s/alt :arity-1 ::cs/args+body
-                    :arity-n (s/cat :bodies (s/+ (s/spec ::cs/args+body))
-                                    :attr (s/? map?)))))
+         :bs ::bodies))
 
 (comment
   (s/fdef defns
--- test/spec_sugar/core_test.clj
+++ test/spec_sugar/core_test.clj
@@ -1,7 +1,8 @@
 (ns spec-sugar.core-test
   (:require [clojure.test :refer :all]
             [spec-sugar.core :as ss]
-            [clojure.spec :as s]))
+            [clojure.spec :as s]
+            [clojure.core.specs :as cs]))
 
 (deftest typed-args
   (testing "We can parse arguments that are typed"
@@ -16,3 +17,54 @@
       '[x]
       '[x :- :a]
       '[x :-])))
+(deftest maybe-typed-args
+  (testing "We can parse arguments that are typed"
+    (are [x] (s/valid? ::ss/maybe-typed-args x)
+      '[x :- integer?]
+      '[x :- boolean?]
+      '[xs :- (s/* integer?)]
+      '[x :- ::int]
+      '[{:keys [a b]} :- (s/* integer?)]
+      '[x :- integer? y]
+      '[x :- boolean? y :- integer?]
+      '[xs :- (s/* integer?) y c d :- integer?]
+      '[x :- ::int d :- ::int e f :- (s/* integer?)]
+      '[{:keys [a b]} :- (s/* integer?) ops second-ops :- map?])
+    (are [x] (not (s/valid? ::ss/maybe-typed-args x))
+      '[1 x]
+      '[x y :-]
+      '[x :- :a y :-]
+      '[x :-])))
+
+(deftest defns-parsing
+  (testing "We can parse full normal defn"
+    (are [x] (s/valid? ::ss/defn-args x)
+      '(add [x y] (+ x y))
+      '(add "with-doc" [x y] (+ x y))
+      '(add ([x y] (+ x y)) ([x y z] (+ x y z)))
+      ))
+  (testing "and one with optional types"
+    (are [x] (s/valid? ::ss/defn-args x)
+      '(add :- integer?
+            [x :- ::int y]
+            (+ x y))
+      '(add
+            [x :- ::int y]
+            (+ x y))
+      '(add :- integer?
+            [x y]
+            (+ x y))
+      '(add :- integer?
+            "docstring"
+            [x :- ::int y]
+            (+ x y))
+      '(add
+            "docstring"
+            [x :- ::int y]
+            (+ x y))
+      '(add :- integer?
+            "docstring"
+            [x :- ::int y :- integer?]
+            (+ x y))
+
+      )))
